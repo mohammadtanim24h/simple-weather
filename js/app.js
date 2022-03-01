ba3df6986ce76fab83ddc289ca3f4a925a09e062
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-01 23:47:39 +0600

    city name added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const searchTemp = () => {
-    const city = document.getElementById('city-name').value;
+    const city = document.getElementById('city-name-field').value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=35b0c1775eb6b6975fa5ef5df721589c&units=metric`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temp => {
+    setInnerText('city-name', temp.name);
 }
